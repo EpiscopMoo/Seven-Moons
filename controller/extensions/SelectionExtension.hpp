83ee113953d2238be93fd5ae69f46922ce9e350f
EpiscopMoo <episcopmoo@gmail.com>
2019-02-19 00:38:56 +0300

    Visibility range fixes

--- controller/extensions/SelectionExtension.hpp
+++ controller/extensions/SelectionExtension.hpp
@@ -41,22 +41,38 @@ public:
                 send_signal(new StageChangeSignal(new_stage));
                 sel_position = game_map->get_pointer_position();
             } else if (get_stage() == Stage::main_game_selection) {
+
+                auto orig = game_map->get_player()->get_position();
+                int fov = game_map->get_player()->get_base_stats().fov;
+                Point dest;
                 switch (key) {
                     case 'k':
                     case KEY_UP:
-                        sel_position.y -= 1;
+                        dest = sel_position - Point({0, 1});
+                        if (VisibilityCalculator::is_visible(orig, dest, fov) and game_map->is_valid(dest)) {
+                            sel_position.y -= 1;
+                        }
                         break;
                     case 'j':
                     case KEY_DOWN:
-                        sel_position.y += 1;
+                        dest = sel_position + Point({0, 1});
+                        if (VisibilityCalculator::is_visible(orig, dest, fov) and game_map->is_valid(dest)) {
+                            sel_position.y += 1;
+                        }
                         break;
                     case 'h':
                     case KEY_LEFT:
-                        sel_position.x -= 1;
+                        dest = sel_position - Point({1, 0});
+                        if (VisibilityCalculator::is_visible(orig, dest, fov) and game_map->is_valid(dest)) {
+                            sel_position.x -= 1;
+                        }
                         break;
                     case 'l':
                     case KEY_RIGHT:
-                        sel_position.x += 1;
+                        dest = sel_position + Point({1, 0});
+                        if (VisibilityCalculator::is_visible(orig, dest, fov) and game_map->is_valid(dest)) {
+                            sel_position.x += 1;
+                        }
                         break;
                     default:
                         break;
@@ -65,15 +81,17 @@ public:
         });
 
         subscribe(Stage::main_game_selection, SignalType::render_prepare, [this] (Signal* signal) {
+            P_Player player = game_map->get_player();
             P_Character selected_character = game_map->character_at(sel_position);
             Point sel_pos_relative_to_window = main_window->get_center() + sel_position - game_map->get_pointer_position();
+
             if (selected_character != nullptr) {
                 left_window->titled_text(selected_character->get_name(), selected_character->get_description());
                 Colors::color_on(main_window->raw(), Colors::YELLOW_BLACK);
                 main_window->print_at(sel_pos_relative_to_window, selected_character->get_skin());
                 Colors::color_off(main_window->raw());
 
-                if (selected_character != game_map->get_player()) {
+                if (selected_character != player) {
                     Colors::color_on(right_window->raw(), Colors::RED_BLACK);
                     right_window->content_at({1, 1}, selected_character->stats().to_strings());
                     Colors::color_off(right_window->raw());
